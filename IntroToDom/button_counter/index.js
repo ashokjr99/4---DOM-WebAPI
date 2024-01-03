d7012a92cb0d76f89b1db7732c99aa4702394a95
ashokjr99 <ashoksharmajr@gmail.com>
2024-01-02 18:20:26 -0600

    prac

---
+++ IntroToDom/button_counter/index.js
@@ -0,0 +1,20 @@
+let incButton = document.getElementById("increase-button");
+let decButton = document.getElementById("decrease-button");
+let countText = document.getElementById("count");
+
+// console.log(incButton);
+
+// incButton.textContent = "increase-count";
+
+let num = 0;
+countText.textContent = num;
+
+incButton.addEventListener("click", () => {
+  num++;
+  countText.textContent = num;
+});
+
+decButton.addEventListener("click", () => {
+  num--;
+  countText.textContent = num;
+});
